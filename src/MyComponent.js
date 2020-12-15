2a7212fe0f63afeaa52b830bcab3697b276ff22b
Rajasegar <rajasegar.c@gmail.com>
2020-12-15 10:25:03 +0530

    chore: first commit

---
+++ src/MyComponent.js
@@ -0,0 +1,49 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import {
+  createTemplate,
+  setComponentTemplate,
+  templateOnlyComponent,
+  getOwner
+} from '@glimmer/core';
+
+import { on, action } from '@glimmer/modifier';
+import OtherComponent from './OtherComponent.js';
+
+
+class MyComponent extends Component {
+  message = 'hello world';
+  @tracked count = 55;
+
+  get currentLocale() {
+    return getOwner(this).services.locale.currentLocale;
+  }
+
+  @action
+  increment() {
+    this.count++;
+  }
+}
+
+const TemplateOnlyComponent = setComponentTemplate(
+  createTemplate(`<h1>I am rendered by a template only component: {{@name}}</h1>`),
+  templateOnlyComponent()
+);
+
+
+setComponentTemplate(
+  createTemplate(
+    { TemplateOnlyComponent, OtherComponent, on },
+    `
+      <h1>Hello {{this.message}}</h1> <br/>
+      <p>{{this.count}}</p>
+      <p>Current Locale: {{this.currentLocale}}</p>
+      <button {{on "click" this.increment}}>Increment</button>
+      <TemplateOnlyComponent @name="Glimmer"/>
+      <OtherComponent @count="22"/>
+    `
+  ),
+  MyComponent
+);
+
+export default MyComponent;
