f081d3e09a56f810238f4836f548f9107dab7770
Rajasegar <rajasegar.c@gmail.com>
2020-12-28 11:49:39 +0530

    chore: implement declarative routing with services

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from '@glimmer/core';
 
 import { on, action } from '@glimmer/modifier';
-import { Router } from './GlimmerRouter.js';
+import { Router, Route } from './GlimmerRouter.js';
 
 import './App.css';
 import logo from './logo.svg';
@@ -22,7 +22,7 @@ class App extends Component {
 
 setComponentTemplate(
   createTemplate(
-    { Router },
+    { Router, Route },
     `
       <img src={{this.logo}} width="128"/>
       <nav>
@@ -30,9 +30,19 @@ setComponentTemplate(
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
+        <li><a href="/fruits?name=apple">Fruits - Apple</a></li>
+        <li><a href="/fruits?name=orange">Fruits - Orange</a></li>
+        <li><a href="/vegetables/1">Vegetables - 1</a></li>
+        <li><a href="/vegetables/2">Vegetables - 2</a></li>
+        <li><a href="/xyz">(404) Not Found</a></li>
       </ul>
       </nav>
-      <Router/>
+      <Route @path="/" @component="Home"/>
+      <Route @path="/about" @component="About"/>
+      <Route @path="/contact" @component="Contact"/>
+      <Route @path="/fruits" @component="Fruits"/>
+      <Route @path="/vegetables/:id" @component="Vegetables"/>
+      <Router></Router>
     `
   ),
   App
